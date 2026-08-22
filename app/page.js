76ecd7a542bac1eac2f545b0c0b04a8ddec60e53
samyleandro1-wq <samyleandro1@gmail.com>
2026-08-22 13:28:45 -0300

    Delete app/page.js

--- app/page.js
+++
@@ -1,4 +0,0 @@
-"use client";
-export default function Home(){
-  return <div style={{background:"black",color:"white",minHeight:"100vh",display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column"}}><h1 style={{fontSize:"50px",fontWeight:"900"}}>CORTA AI ✂️</h1><p>SITE NO AR - AGORA VAMOS COLOCAR O BONITO</p></div>
-}
