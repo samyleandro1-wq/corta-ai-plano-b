06a5f5711c812a6c0de658b9da6527437045f75e
samyleandro1-wq <samyleandro1@gmail.com>
2026-08-24 16:41:43 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -131,6 +131,9 @@ a.remove();
                   <button onClick={() => baixarVideo(c.id || videoId, idx)} className="bg-purple-600 px-3 py-1 rounded text-sm font-bold">{baixandoId === (c.id + "-" + idx) || baixandoId === (videoId + "-" + idx) ? "..." : "Baixar"}</button>
                 </div>
               </div>
-            ))}
-          </div>
-        )}
+                  ))}
+      </div>
+    )}
+  </>
+  )
+} 
